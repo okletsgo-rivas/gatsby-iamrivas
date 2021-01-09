be93446366851a03291111a1de83f5d8f4be0a92
John Rivas <okletsgo@gmail.com>
2021-01-09 02:30:30 -0800

    Project groups fixed

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 // If you don't want to use TypeScript you can delete this file!
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import {
   Container,
@@ -13,8 +13,19 @@ import {
 import Layout from "../components/layout"
 
 const Projects = ({ data }) => {
-  const filterHandler = val => {
-    console.log(this, val)
+  const [state, setState] = useState({
+    filteredData: data.projects.edges,
+  })
+
+  const filterHandler = id => {
+    let filteredData = data.projects.edges
+    if (id !== "all") {
+      filteredData = filteredData.filter(({ node }) =>
+        node.relationships.field_type.find(tag => tag.id === id)
+      )
+    }
+    console.log(filteredData)
+    setState({ filteredData })
   }
 
   return (
@@ -22,12 +33,13 @@ const Projects = ({ data }) => {
       <Container className="projects">
         <Row className="filters">
           <ToggleButtonGroup
+            defaultValue="all"
             name="filters"
             className="w-100"
             onChange={filterHandler}
             aria-label="Filter Group"
           >
-            <ToggleButton varient="secondary" type="radio">
+            <ToggleButton value="all" variant="secondary" type="radio">
               ALL
               <Badge variant="light" className="ml-2">
                 {data.projects.edges.length}
@@ -48,7 +60,7 @@ const Projects = ({ data }) => {
             ))}
           </ToggleButtonGroup>
         </Row>
-        {data.projects.edges.map(({ node }) => (
+        {state.filteredData.map(({ node }) => (
           <Row className="react-reveal project" key={node.id}>
             <Col
               md={6}
@@ -132,6 +144,9 @@ export const query = graphql`
               image_style_uri {
                 projects
               }
+            }
+            field_type {
+              id
             }
           }
           id
